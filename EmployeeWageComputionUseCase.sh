b3417e31c4f246a726d69d6629c326716ad9dbed
Nileshlad <ladnilesh1994@gmail.com>
2020-02-14 17:31:30 +0530

    ADDED:To part time employe and wage

--- EmployeeWageComputionUseCase.sh
+++ EmployeeWageComputionUseCase.sh
@@ -12,15 +12,15 @@ randomNumber=$((RANDOM%2))
 if [ $randomNumber -eq 	$isPresent ]
 then
 	echo "Employee Present"
-  	#TO FUNCTION CALCULATE EMPLOYEE WAGE
-	function calculateDailyEmployeeWae(){
+  	#TO FUNCTION PART TIME EMPLOYEE AND  WAGE
+	function partTimeEmployeeAndWage(){
 	read -p "Assume Wage per hour is :" isWagePerHour "rupees"
-        isDayHour=24
- 	read -p "To full day hours is :" isFullDayHour
-	calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
-	echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
+	isDayHour=24
+ 	read -p "To part time hour is :" isFullDayHour
+	calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
+	echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage "rupees"
 }
-calculateDailyEmployeeWae
+partTimeEmployeeAndWage
 else
    echo "Employee Absent"
  fi
