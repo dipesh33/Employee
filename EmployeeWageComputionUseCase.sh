0999537dee0c905fff7f2773430bf7da36e7aeda
Nileshlad <ladnilesh1994@gmail.com>
2020-02-15 12:14:36 +0530

    MODIFIED:To remove the data

--- EmployeeWageComputionUseCase.sh
+++ EmployeeWageComputionUseCase.sh
@@ -19,8 +19,7 @@ case "$caseChoce" in
 		#TO FUNCTION CALCULATE EMPLOYEE WAGE
 		function calculateDailyEmployeeWae(){
 		read -p "Assume Wage per hour is :" isWagePerHour "rupees"
-		isDayHour=24
- 		read -p "To full day hours is :" isFullDayHour
+		read -p "To full day hours is :" isFullDayHour
 		calculateEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
 		echo "To calculate daily employee wage :" $calculateEmployeeWage "rupees"
 		}
@@ -29,8 +28,7 @@ case "$caseChoce" in
 	1)echo "To calculate part time Employee and wage"
 		function partTimeEmployeeAndWage(){
 		read -p "Assume Wage per hour is :" isWagePerHour "rupees"
-		isDayHour=24
- 		read -p "To part time hour is :" isFullDayHour
+		read -p "To part time hour is :" isFullDayHour
 		calculatePartTimeEmployeeWage=$(( $isWagePerHour * $isFullDayHour ))
 		echo "To calculate part time employee and wage :" $calculatePartTimeEmployeeWage "rupees"
 			}
